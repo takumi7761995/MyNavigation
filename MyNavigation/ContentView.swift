773cbdafafac78655fecc3c2ff800aea182faaa9
杉本匠 <takumi1995776@gmail.com>
2022-04-09 00:42:53 +0900

    Initial Commit

---
+++ MyNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNavigation
+//
+//  Created by 杉本匠 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNavigation/MyNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNavigationApp.swift
+//  MyNavigation
+//
+//  Created by 杉本匠 on 2022/04/09.
+//
+
+import SwiftUI
+
+@main
+struct MyNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
