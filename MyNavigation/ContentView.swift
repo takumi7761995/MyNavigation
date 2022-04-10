4e292d0fa3140a68cda5663697346731eda00f67
杉本匠 <takumi1995776@gmail.com>
2022-04-10 13:10:35 +0900

    Content コメント

--- MyNavigation/ContentView.swift
+++ MyNavigation/ContentView.swift
@@ -7,38 +7,38 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    private let defaultTag: Int = 8888
-    @State var destination: AnyView? = nil
-    @State var tag: Int? = nil
-
-    var body: some View {
-        NavigationView {
-            VStack  {
-                Button(action: {
-                    self.destination = AnyView(Text("child 1"))
-                    self.tag = self.defaultTag
-                }) {
-                    Text("tap 1")
-                }
-                Button(action: {
-                    self.destination = AnyView(Text("child 2"))
-                    self.tag = self.defaultTag
-                }) {
-                    Text("tap 2")
-                }
-                NavigationLink(destination: destination, tag: defaultTag, selection: $tag) {
-                    EmptyView()
-                }
-            }
-        }
-    }
-}
-
-
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView: View {
+//  private let defaultTag: Int = 8888
+//  @State var destination: AnyView? = nil
+//  @State var tag: Int? = nil
+//
+//  var body: some View {
+//    NavigationView {
+//      VStack  {
+//        Button(action: {
+//          self.destination = AnyView(Text("child 1"))
+//          self.tag = self.defaultTag
+//        }) {
+//          Text("tap 1")
+//        }
+//        Button(action: {
+//          self.destination = AnyView(Text("child 2"))
+//          self.tag = self.defaultTag
+//        }) {
+//          Text("tap 2")
+//        }
+//        NavigationLink(destination: destination, tag: defaultTag, selection: $tag) {
+//          EmptyView()
+//        }
+//      }
+//    }
+//  }
+//}
+//
+//
+//
+//struct ContentView_Previews: PreviewProvider {
+//  static var previews: some View {
+//    ContentView()
+//  }
+//}
